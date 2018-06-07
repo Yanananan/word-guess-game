99b86c68bd7970c9e52639afe1cc935d5e6b208a
Yanananan <meng.yanan@gmail.com>
2018-06-07 02:36:25 -0700

    added youtube playback code, need to test via github link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
+
 function selectItem (){
     //returns a random item out of array of items
     var itemArray = ["Taylor Swift", "Bruno Mars", "Rihanna"];
     var item = itemArray[Math.floor(Math.random() * itemArray.length)];
-    console.log(item);
+    // console.log(item);
     return item;
 }
 
@@ -39,41 +40,76 @@ function setCharAt(string,i,newChar) {
 
 function recordKey(event){
     //main function
-    var key = event.key;
-    console.log("before check key = "+ key);
-    key = checkKey(key);
-    console.log("after check key = "+ key);
-    if (item.toLowerCase().indexOf(key)<0){  //checks key against lower case version of the answer
-        if (wrongLetters.indexOf(key)<0){  //checks key against list of wrong letters/numbers guessed
-            console.log("wrongLetters = "+wrongLetters);
-            console.log("wrongLetters.indexOf(key) = "+wrongLetters.indexOf(key));
-            numberOfGuesses++;
-            var guesses = document.querySelector('#guesses');
-            guesses.innerText = "The number of guesses is "+ (numberOfGuesses);
-            var letters = document.querySelector('#letters');
-            wrongLetters=wrongLetters+key;
-            letters.innerText = "The letters guessed are "+wrongLetters;
-        }
-    } else {
-        for (var i=0; i<item.length; i++){
-            // console.log("i = "+ i);
-            // console.log("item[i] = "+ item[i]);
-            if (key==item.toLowerCase()[i]){
-                // console.log("blankItem before = "+ blankItem);
-                blankItem = setCharAt(blankItem,i,item[i]);
-                // console.log("blankItem after = "+ blankItem);
+    if (numberOfGuessesLeft==0||blankItem.indexOf("_")<0){ //number of guesses have run out or game is won
+        item = selectItem();
+        blankItem = startItem(item);
+        var answer = document.querySelector('#answer');
+        answer.innerText = "The answer is ...";
+        var guesses = document.querySelector('#guesses');
+        guesses.innerText = "The number of guesses is 10";
+        numberOfGuessesLeft=10;
+        var letters = document.querySelector('#letters');
+        letters.innerText = "The letters guessed are ...";
+        wrongLetters = "";
+        var winLose = document.querySelector('#winLose');
+        winLose.innerText = "";
+    } else { //game continues
+        var key = event.key;
+        console.log("before check key = "+ key);
+        key = checkKey(key);
+        console.log("after check key = "+ key);
+        if (item.toLowerCase().indexOf(key)<0){  //checks key against lower case version of the answer
+            if (wrongLetters.indexOf(key)<0){  //checks key against list of wrong letters/numbers guessed
+                console.log("wrongLetters = "+wrongLetters);
+                console.log("wrongLetters.indexOf(key) = "+wrongLetters.indexOf(key));
+                numberOfGuessesLeft--;
+                var guesses = document.querySelector('#guesses');
+                guesses.innerText = "The number of guesses is "+ (numberOfGuessesLeft);
+                var letters = document.querySelector('#letters');
+                wrongLetters=wrongLetters+key;
+                letters.innerText = "The letters guessed are "+wrongLetters;
+            }
+        } else {
+            for (var i=0; i<item.length; i++){
+                // console.log("i = "+ i);
+                // console.log("item[i] = "+ item[i]);
+                if (key==item.toLowerCase()[i]){
+                    // console.log("blankItem before = "+ blankItem);
+                    blankItem = setCharAt(blankItem,i,item[i]);
+                    // console.log("blankItem after = "+ blankItem);
+                }
             }
         }
+        console.log("blankItem = "+ blankItem);
+        var answer = document.querySelector('#answer');
+        answer.innerText = "The answer is "+blankItem;
+
+        if (blankItem.indexOf("_")<0){ //answer was successfully guessed
+            var winLose = document.querySelector('#winLose');
+            winLose.innerText = "You win!";
+        }
+    
+        if (numberOfGuessesLeft==0){ //number of guesses have run out
+            var winLose = document.querySelector('#winLose');
+            winLose.innerText = "You lose!";
+        }
     }
-    console.log("blankItem = "+ blankItem);
-    var answer = document.querySelector('#answer');
-    answer.innerText = "The answer is "+blankItem;
-    
-    
 }
 
-var item = selectItem();
-var blankItem = startItem(item);
-var numberOfGuesses = 0;
-var wrongLetters = "";
+
+var numberOfGuessesLeft = 0;
 document.onkeyup = recordKey;
+
+//rihanna diamond https://www.youtube.com/watch?v=5f2mZSk-nvU
+//taylor trouble  https://www.youtube.com/watch?v=3gxPgU2-ih4
+//Idina Menzel  let it go     https://www.youtube.com/watch?v=A_DaizJnnJQ
+//Adele rolling in the deep   https://www.youtube.com/watch?v=3Nxxo9E2_x8
+//LMFAO sexy and i know it https://www.youtube.com/watch?v=K1GKCBYtf1M
+//bieber baby https://www.youtube.com/watch?v=ZH9MDQSH_Z0
+//Adell Hello  https://www.youtube.com/watch?v=wq3zEOcXyGA
+//one direction what makes you beautiful https://www.youtube.com/watch?v=aqXM0OzsDn0
+//justin timberlake sexyback  https://www.youtube.com/watch?v=zgxPkjhdr-U
+//michael jackson thriller  https://www.youtube.com/watch?v=XCr6kR2UK4k
+//katy perry firework https://www.youtube.com/watch?v=Oq_OvVavrN8
+//alicia keys girl on fire https://www.youtube.com/watch?v=bSJSU9GPzHM
+//miley cyrus party in the usa  https://www.youtube.com/watch?v=rPzqF2pzCKE
